1ef3c9e61131a1c6d653eab7cb8dbf663257a675
Viktor Kalabukhov <kalabukhov87@gmail.com>
2025-05-21 13:52:44 +0300

    add templates

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import pymorphy2
 
 app = Flask(__name__)
 CORS(app)
-
 morph = pymorphy2.MorphAnalyzer()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -21,10 +24,8 @@ def analyze():
             results[word] = ["(не найдено)"]
             continue
 
-        # Берем наиболее вероятный вариант
         best = parsed[0]
 
-        # Лексема = все формы этого слова
         try:
             forms = sorted(set([form.word for form in best.lexeme]))
             results[word] = forms
@@ -34,4 +35,4 @@ def analyze():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
